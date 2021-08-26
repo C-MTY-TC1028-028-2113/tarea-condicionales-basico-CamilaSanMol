62ec00c09a00d39e4cff3df358f12ae33cda6075
Sofia Camila Santiago Molina <A01704294@itesm.mx>
2021-08-26 18:36:47 -0500

    tarea condicionales

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,6 +1,22 @@
 def main():
     #escribe tu código abajo de esta línea
+    peso=float(input("Peso en kg: "))
+    altura=float(input("Altura en m: "))
     
-
+    if (peso<=0 or altura<=0):
+        print('Revisa tus datos, alguno de ellos es erróneo.')
+    else:
+        imb=peso/altura**2
+        if (imb>0 and imb<20):
+            print('PESO BAJO')
+        elif (imb>=20 and imb<25):
+            print('NORMAL')
+        elif (imb>=25 and imb<30):
+            print('SOBREPESO')
+        elif (imb>=30 and imb<40):
+            print('OBESIDAD')
+        elif (imb>=40):
+            print('OBESIDAD MORBIDA')
+    
 if __name__=='__main__':
     main()
